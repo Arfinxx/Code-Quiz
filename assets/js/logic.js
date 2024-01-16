bc1cb55b327216adae0d9d4dabd50653cf05607f
Arfinxx <arfin@hotmail.co.uk>
2024-01-16 19:41:19 +0000

    Added JS for making question and answer appear when start button clicked

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,5 +1,7 @@
 // Variables
+var startScreen = document.querySelector("#start-screen");
 var startButton = document.querySelector("#start");
+var questionEl = document.querySelector("#questions");
 var questionTitle = document.querySelector("#question-title");
 var questionChoices = document.querySelector("#choices");
 var timerEl = document.querySelector("#time");
@@ -7,7 +9,9 @@ var timerEl = document.querySelector("#time");
 
 // When the start button is pressed a timer starts
 var timeLeft = 30;
-function CountdownTimer () {
+var questionNumber = 0;
+var answerArr = questions[questionNumber].answers;
+function countdownTimer () {
     
 
     var timeInterval = setInterval(function () {
@@ -23,13 +27,34 @@ function CountdownTimer () {
     }, 1000);
 };
 
+
 startButton.addEventListener("click", function() {
 
-    CountdownTimer();
-    
+    countdownTimer();
+// Then a question appears with 4 answer options
+if(questionEl.getAttribute("class") === "hide"){ 
+    startScreen.textContent = ``;
+
+    questionTitle.textContent = questions[questionNumber].title
+   for(i = 0; i < answerArr.length; i++) { 
+    var node = document.createElement("li");
+    var textnode = document.createTextNode(answerArr[i]);
+    node.appendChild(textnode);
+    questionChoices.appendChild(node);
+    console.log(questionChoices);
+   }
+
+
+
+    questionEl.setAttribute("class", "show");
+} else {
+    questionTitle.textContent = ``;
+    questionEl.setAttribute("class", "hide");
+}
 });
 
-// Then a question appears with 4 answer options
+
+
 // When an answer is clicked it tells the user if it is correct or incorrect
 // When an right answer is clicked the score changes
 // When an incorrect answer is clicked the time is reduced
---
+++ assets/js/questions.js
@@ -0,0 +1,30 @@
+var questions = [
+
+    {
+    title: "What does HTML stand for?" ,
+    answers: ["Hyper-Text Markup Language", "Hidden Text Mock Language", "Hyper-Test Markup Language", "Happy Tanks Must Laugh"]
+},
+    {
+    title: "Which part of the HTML file contains the Metadata of the webpage?" ,
+    answers: ["<body>", "<meta>", "<head>", "<h1>"]
+},
+
+// TODO: create and add questions.
+    {
+    title: "" ,
+    answers: ["<body>", "<meta>", "<head>", "<h1>"]
+},
+    {
+    title: "Which part of the HTML file contains the Metadata of the webpage?" ,
+    answers: ["<body>", "<meta>", "<head>", "<h1>"]
+},
+    {
+    title: "Which part of the HTML file contains the Metadata of the webpage?" ,
+    answers: ["<body>", "<meta>", "<head>", "<h1>"]
+},
+    {
+    title: "Which part of the HTML file contains the Metadata of the webpage?" ,
+    answers: ["<body>", "<meta>", "<head>", "<h1>"]
+},
+
+];
