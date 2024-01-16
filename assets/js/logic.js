26d2b2def664fe77893c21427f6079b7cce8e8f2
Arfinxx <arfin@hotmail.co.uk>
2024-01-16 17:58:22 +0000

    Added Pseudocode to logic.js

---
+++ assets/js/logic.js
@@ -0,0 +1,12 @@
+// When the start button is pressed a timer starts
+// Then a question appears with 4 answer options
+// When an answer is clicked it tells the user if it is correct or incorrect
+// When an right answer is clicked the score changes
+// When an incorrect answer is clicked the time is reduced
+// The next question appears.
+// repeats until all questions have been answered, or time runs out
+// Gives the user the option of entering their intials
+
+
+// Saves their score locally and lists it on the "highscores" page.
+// There is a button with the option of resetting the leaderboard. 
