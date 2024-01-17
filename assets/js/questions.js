3956e29514537cf59a34b2af8b083b3482fbf387
Arfinxx <arfin@hotmail.co.uk>
2024-01-17 00:05:05 +0000

    added more questions

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -8,27 +8,33 @@ var questions = [
     title: "Which part of the HTML file contains the Metadata of the webpage?" ,
     answers: ["<body>", "<meta>", "<head>", "<h1>"]
 },
-
-// TODO: create and add questions.
-//     {
-//     title: "" ,
-//     answers: ["<body>", "<meta>", "<head>", "<h1>"]
-// },
-//     {
-//     title: "Which part of the HTML file contains the Metadata of the webpage?" ,
-//     answers: ["<body>", "<meta>", "<head>", "<h1>"]
-// },
-//     {
-//     title: "Which part of the HTML file contains the Metadata of the webpage?" ,
-//     answers: ["<body>", "<meta>", "<head>", "<h1>"]
-// },
-//     {
-//     title: "Which part of the HTML file contains the Metadata of the webpage?" ,
-//     answers: ["<body>", "<meta>", "<head>", "<h1>"]
-// },
+    {
+    title: "What attribute tag can be used to open a link in a new tab?" ,
+    answers: ["class", "data", "src", "target"]
+},
+    {
+    title: "Which is the largest heading tag?" ,
+    answers: ["<h3>", "<p>", "<h6>", "<h1>"]
+},
+    {
+    title: "What symbol is used to identify a css class selector?" ,
+    answers: ["/", "#", ".", "<"]
+},
+    {
+    title: "Which CSS attribute can be used to horizontally center a div?" ,
+    answers: ["horizontal-center", "align-items", "justify-content", "center-items"]
+},
+    {
+    title: "what is used to create Dynamic content/webpages?" ,
+    answers: ["Javascript", "BootStrap", "HTML", "CSS"]
+},
+    {
+    title: "what what can be used to excecute some code only if a certain condition is true?" ,
+    answers: ["true loop", "IF loop", "FOR loop", "run"]
+}
 
 ];
 
 // array containing the array index for the correct answers for each question:
 
-var correctArr = [0, 2, , , , , ];
+var correctArr = [0, 2, 3, 3, 2, 2, 0, 1];
